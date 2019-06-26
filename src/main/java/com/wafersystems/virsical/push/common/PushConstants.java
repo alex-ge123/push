883ce66ee7537b3ea24e9ce2dfa1a72aaed3fd6a
tandongkui <tandongkui@wafersystems.com>
2019-06-26 14:07:03 +0800

    常量定义转移到class；

--- src/main/java/com/wafersystems/virsical/push/common/PushConstants.java
+++ src/main/java/com/wafersystems/virsical/push/common/PushConstants.java
@@ -6,20 +6,20 @@ package com.wafersystems.virsical.push.common;
  * @author tandk
  * @date 2019/6/10 10:45
  */
-public interface PushConstants {
+public final class PushConstants {
 
   /**
    * 广播群发，推送所有目的地
    */
-  String PUSH_ALL_DESTINATION = "/topic/all";
+  public static final String PUSH_ALL_DESTINATION = "/topic/all";
 
   /**
    * 广播群发，推送指定产品目的地
    */
-  String PUSH_PRODUCT_DESTINATION = "/topic/";
+  public static final String PUSH_PRODUCT_DESTINATION = "/topic/";
 
   /**
    * 广播订阅，推送指定用户目的地
    */
-  String PUSH_ONE_DESTINATION = "/one";
+  public static final String PUSH_ONE_DESTINATION = "/one";
 }
