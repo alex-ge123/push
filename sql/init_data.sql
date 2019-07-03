61392de5a8a14d9294c1f774d05f26acbf3480fd
tandongkui <tandongkui@wafersystems.com>
2019-07-03 09:35:34 +0800

    加入网关配置sql；

---
+++ sql/init_data.sql
@@ -0,0 +1,9 @@
+
+-- ----------------------------
+-- Records of virsical_base.sys_route_conf
+-- ----------------------------
+INSERT INTO `sys_route_conf` VALUES (null, '推送服务', 'push', '[{\"args\": {\"_genkey_0\": \"/push/**\"}, \"name\": \"Path\"}]', '[]', 'lb://virsical-push', 0, '2019-07-03 10:00:00', '2019-07-03 10:00:00', '0');
+INSERT INTO `sys_route_conf` VALUES (null, '推送长链接支持1', 'push-ws-1', '[{\"args\": {\"_genkey_0\": \"/push/ws/info/**\"}, \"name\": \"Path\"}]', '[]', 'lb://virsical-push', 0, '2019-07-03 10:00:00', '2019-07-03 10:00:00', '0');
+INSERT INTO `sys_route_conf` VALUES (null, '推送长链接支持2', 'push-ws-2', '[{\"args\": {\"_genkey_0\": \"/push/ws/**\"}, \"name\": \"Path\"}]', '[]', 'lb:ws://virsical-push', 0, '2019-07-03 10:00:00', '2019-07-03 10:00:00', '0');
+
+
