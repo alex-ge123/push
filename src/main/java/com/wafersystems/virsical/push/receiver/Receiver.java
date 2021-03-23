469e0d1afcc7bf2c6e9d9518964f7ec5d83d455b
tandongkui <tandongkui@wafersystems.com>
2021-03-23 09:44:37 +0800

    Changed 升级公共包版本6.4.103；消息消费日志加入bizId

--- src/main/java/com/wafersystems/virsical/push/receiver/Receiver.java
+++ src/main/java/com/wafersystems/virsical/push/receiver/Receiver.java
@@ -1,11 +1,13 @@
 package com.wafersystems.virsical.push.receiver;
 
 import com.alibaba.fastjson.JSON;
+import com.wafersystems.virsical.common.core.constant.CommonConstants;
 import com.wafersystems.virsical.common.core.constant.enums.EnvEnum;
 import com.wafersystems.virsical.common.core.constant.enums.MsgTypeEnum;
 import com.wafersystems.virsical.common.core.dto.MessageDTO;
 import com.wafersystems.virsical.push.common.PushConstants;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -38,14 +40,18 @@ public class Receiver {
    */
   @RabbitListener(queues = "${push.fanout.queue}")
   public void receiveTopicPush(@Payload String message) {
-    log.info("【push.fanout.queue监听到消息】{}", message);
     try {
       // 服务首次消费延时10秒，为了避免在服务刚启动后，WebSocket连接还未建立的情况下，进行消费
       if (isFirst && !EnvEnum.TESTCASE.getType().equals(springProfilesActive)) {
+        log.warn("服务首次启动消费延时10秒，为了避免在服务刚启动后，WebSocket连接还未建立的情况下进行消费。开始...");
         Thread.sleep(10000);
         isFirst = false;
+        log.warn("延时10秒结束");
       }
       MessageDTO messageDTO = JSON.parseObject(message, MessageDTO.class);
+      // 业务追踪id
+      MDC.put(CommonConstants.LOG_BIZ_ID, messageDTO.getBizId());
+      log.info("【push.fanout.queue监听到消息】{}", message);
       if (MsgTypeEnum.ALL.name().equals(messageDTO.getMsgType())) {
         simpMessagingTemplate.convertAndSend(PushConstants.PUSH_ALL_DESTINATION, messageDTO);
       } else if (MsgTypeEnum.ONE.name().equals(messageDTO.getMsgType())) {
